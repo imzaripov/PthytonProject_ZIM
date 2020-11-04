30e3c2bf4bbbd168727349d9ba956d817941002b
imzaripov <imzaripov@icloud.com>
2020-11-04 19:26:10 +0300

    delete directory

--- lesson_1/test_3.py
+++
@@ -1,6 +0,0 @@
-n = int(input('Задайте число n:'))
-n1 = str(n)
-n2 = n1 + n1
-n3 = n1 + n1 + n1
-s = n + int(n2) + int(n3)
-print('Решение:', s)
--- lesson_1/test_4.py
+++
@@ -1,8 +0,0 @@
-n = int(input('Задайте число n:'))
-max = n % 10
-n = n // 10
-while n > 0:
-    if n % 10 > max:
-        max = n % 10
-    n = n // 10
-print('Самая большая цифра -', max)
