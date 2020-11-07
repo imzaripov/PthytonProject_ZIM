8ffab68f525a71f36e3159aa5f3456dbd7b7e7da
imzaripov <imzaripov@icloud.com>
2020-11-07 09:35:11 +0300

    delete files

--- lesson_2/test_2_1.py
+++
@@ -1,4 +0,0 @@
-ls = [34, 'текст', 45.5, True, abs(-7), complex(3, 6), (34, 'test', None), set('abc'),
-      {'Paleozoic': 'Dev,Car,Per', 'Mesozoic': 'Tri,Jur,Cre'}]
-for el in ls:
-    print(f'{el} is {type(el)}')
